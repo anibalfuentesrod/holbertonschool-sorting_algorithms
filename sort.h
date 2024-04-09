35cafc3b5cbb084b26632147ef7a9caf0abe0097
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-04-09 11:45:03 -0400

    fixing the betty of the file sort.h

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
@@ -25,5 +25,4 @@ void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-
 #endif
