3836e146a1e3539a81dde5bb91e8ed23c8873032
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-04-09 14:43:56 -0400

    making the advance let's gooo

---
+++ 100-shell_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts an array of int in ascend
+ * @array: array to be sorted
+ * @size: size of the array
+**/
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int temp;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	for (; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
